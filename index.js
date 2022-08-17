7af52a96985d747bc2c3235b3583f07c27a86cc7
metwse <108795071+metwse@users.noreply.github.com>
2022-08-17 17:12:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 app.route('/', (req, res) => res.json('hello world!'))
-app.listen(443)
+var http = require('http');
+
+http.get({'host': 'api.ipify.org', 'port': 80, 'path': '/'}, function(resp) {
+  resp.on('data', function(ip) {
+    console.log("My public IP address is: " + ip);
+  });
+});
+app.listen(80)
